1669106a982bbb115166975528666839d7f6290d
damian-anslik <damiananslik@outlook.com>
2022-04-12 22:53:01 +0100

    move event handlers outside document on ready and update session logic

--- static/site.js
+++ static/site.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
-
-    // Save the users markdown and preferences
-    window.addEventListener('beforeunload', function (e) {
-        sessionStorage.setItem('markdown', $('#markdown').val());
-        localStorage.setItem('fontFamily', $('#fontFamily').val());
-        localStorage.setItem('fontSize', parseInt($('#fontSize').val()));
-        localStorage.setItem('convertFormat', $('#convertFormat').val());
-    });
-
+    
     var markdown = sessionStorage.getItem('markdown');
     var fontFamily = localStorage.getItem('fontFamily');
     var fontSize = localStorage.getItem('fontSize');
@@ -28,44 +20,58 @@ $(document).ready(function () {
     if (convertFormat) {
         $('#convertFormat').val(convertFormat);
     }
-
-    $('#convert').click(function () {
-        const format = $('#convertFormat').val();
-        const markdown = $('#markdown').val();
-        if (format == "md") {
-            download(markdown, "markdown.md");
-        }
-        var converter = new showdown.Converter();
-        var html = converter.makeHtml(markdown);
-        if (format == "html") {
-            download(html, "markdown.html");
-        }
-        if (format == "pdf") {
-            var doc = new jsPDF();
-            doc.fromHTML(html, 15, 15, {
-                'width': 170
-            });
-            doc.save('markdown.pdf');
-        }
-        return false;
-    });
-
-    $('#file-input').change(function () {
-        const file = this.files[0];
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = function (e) {
-            $('#markdown').val(e.target.result);
-        }
-    });
-
-    $('#fontFamily').change(function () {
-        $('#markdown').css('font-family', $('#fontFamily').val());
-    });
-
-    $('#fontSize').change(function () {
-        const fontSize = parseInt($(this).val());
-        $('#markdown').css('font-size', fontSize);
-    });
+    
 });
 
+// Save the users markdown on page refresh
+window.addEventListener('beforeunload', function (e) {
+    var markdown = $('#markdown').val();
+    sessionStorage.setItem('markdown', markdown);
+});
+
+$('#convert').click(function () {
+    const format = $('#convertFormat').val();
+    const markdown = $('#markdown').val();
+    if (format == "md") {
+        download(markdown, "markdown.md");
+    }
+    var converter = new showdown.Converter();
+    var html = converter.makeHtml(markdown);
+    if (format == "html") {
+        download(html, "markdown.html");
+    }
+    if (format == "pdf") {
+        var doc = new jsPDF();
+        doc.fromHTML(html, 15, 15, {
+            'width': 170
+        });
+        doc.save('markdown.pdf');
+    }
+    return false;
+});
+
+$('#file-input').change(function () {
+    const file = this.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function (e) {
+        $('#markdown').val(e.target.result);
+    }
+});
+
+$('#convertFormat').change(function () {
+    const format = $('#convertFormat').val();
+    localStorage.setItem('convertFormat', format);
+});
+
+$('#fontFamily').change(function () {
+    const fontFamily = $('#fontFamily').val();
+    localStorage.setItem('fontFamily', fontFamily);
+    $('#markdown').css('font-family', fontFamily);
+});
+
+$('#fontSize').change(function () {
+    const fontSize = parseInt($(this).val());
+    localStorage.setItem('fontSize', fontSize);
+    $('#markdown').css('font-size', fontSize);
+});
