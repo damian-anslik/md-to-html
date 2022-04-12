02e7255ef0bfdfdb39d5171c690485c08836ac56
damian-anslik <damiananslik@outlook.com>
2022-04-12 20:04:07 +0100

    moved markdown conversion to front-end

--- app/convert.py
+++ app/convert.py
@@ -1,14 +1,7 @@
-from flask import Blueprint, request, render_template
-from markdown import markdown
+from flask import Blueprint, render_template
 
 convert_bp = Blueprint('convert', __name__)
 
 @convert_bp.route('/')
 def index():
     return render_template('convert.html')
-
-@convert_bp.post("/")
-def convert():
-    md_string = request.form.get('markdown')
-    html_string = markdown(md_string)
-    return render_template('converted.html', html=html_string)
--- app/static/site.js
+++ app/static/site.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    
+    $('#convert').click(function(){
+        const format = $('#convertFormat').val();
+        const markdown = $('#markdown').val();
+        if (format=="md") {
+            return download(markdown, "markdown.md");
+        }
+        else if (format=="html") {
+            var converter = new showdown.Converter();
+            var html = converter.makeHtml(markdown);
+            return download(html, "markdown.html");
+        }
+    });
+
 });
+
