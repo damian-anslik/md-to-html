cd1ed70a83977af81e8ef21aec888211e3ad2046
damian-anslik <damiananslik@outlook.com>
2022-04-13 20:01:50 +0100

    allow client to name their document

---
+++ static/handlers.js
@@ -0,0 +1,57 @@
+$('#file-input').change(function () {
+    const file = this.files[0];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function (e) {
+        $('#markdown').val(e.target.result);
+    }
+});
+
+$('#convertFormat').change(function () {
+    const format = $('#convertFormat').val();
+    localStorage.setItem('convertFormat', format);
+});
+
+$('#fontFamily').change(function () {
+    const fontFamily = $('#fontFamily').val();
+    localStorage.setItem('fontFamily', fontFamily);
+    $('#markdown').css('font-family', fontFamily);
+});
+
+$('#fontSize').change(function () {
+    const fontSize = parseInt($(this).val());
+    localStorage.setItem('fontSize', fontSize);
+    $('#markdown').css('font-size', fontSize);
+});
+
+$('.document-title').change(function () {
+    const title = $(this).val();
+    sessionStorage.setItem('title', title);
+});
+
+// Save the users markdown on page refresh
+window.addEventListener('beforeunload', function (e) {
+    var markdown = $('#markdown').val();
+    sessionStorage.setItem('markdown', markdown);
+});
+
+$('#convert').click(function () {
+    const format = $('#convertFormat').val();
+    const markdown = $('#markdown').val();
+    if (format == "md") {
+        download(markdown, "markdown.md");
+    }
+    var converter = new showdown.Converter();
+    var html = converter.makeHtml(markdown);
+    if (format == "html") {
+        download(html, "markdown.html");
+    }
+    if (format == "pdf") {
+        var doc = new jsPDF();
+        doc.fromHTML(html, 15, 15, {
+            'width': 170
+        });
+        doc.save('markdown.pdf');
+    }
+    return false;
+});
--- static/site.js
+++ static/site.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     
     var markdown = sessionStorage.getItem('markdown');
+    var documentTitle = sessionStorage.getItem('title');
     var fontFamily = localStorage.getItem('fontFamily');
     var fontSize = localStorage.getItem('fontSize');
     var convertFormat = localStorage.getItem('convertFormat');
@@ -20,58 +21,8 @@ $(document).ready(function () {
     if (convertFormat) {
         $('#convertFormat').val(convertFormat);
     }
+    if (documentTitle) {
+        $('.document-title').val(documentTitle);
+    }
     
-});
-
-// Save the users markdown on page refresh
-window.addEventListener('beforeunload', function (e) {
-    var markdown = $('#markdown').val();
-    sessionStorage.setItem('markdown', markdown);
-});
-
-$('#convert').click(function () {
-    const format = $('#convertFormat').val();
-    const markdown = $('#markdown').val();
-    if (format == "md") {
-        download(markdown, "markdown.md");
-    }
-    var converter = new showdown.Converter();
-    var html = converter.makeHtml(markdown);
-    if (format == "html") {
-        download(html, "markdown.html");
-    }
-    if (format == "pdf") {
-        var doc = new jsPDF();
-        doc.fromHTML(html, 15, 15, {
-            'width': 170
-        });
-        doc.save('markdown.pdf');
-    }
-    return false;
-});
-
-$('#file-input').change(function () {
-    const file = this.files[0];
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = function (e) {
-        $('#markdown').val(e.target.result);
-    }
-});
-
-$('#convertFormat').change(function () {
-    const format = $('#convertFormat').val();
-    localStorage.setItem('convertFormat', format);
-});
-
-$('#fontFamily').change(function () {
-    const fontFamily = $('#fontFamily').val();
-    localStorage.setItem('fontFamily', fontFamily);
-    $('#markdown').css('font-family', fontFamily);
-});
-
-$('#fontSize').change(function () {
-    const fontSize = parseInt($(this).val());
-    localStorage.setItem('fontSize', fontSize);
-    $('#markdown').css('font-size', fontSize);
 });
