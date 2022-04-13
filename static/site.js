a52aa3d9124a825eb21f6e36d69eb783c7168ef5
damian-anslik <damiananslik@outlook.com>
2022-04-13 23:40:23 +0100

    save user editor textarea height and width

--- static/site.js
+++ static/site.js
@@ -5,23 +5,31 @@ $(document).ready(function () {
         fontSize: localStorage.getItem('fontSize') || '16',
         format: localStorage.getItem('format') || 'md',
     };
-
+    
     var sessionData = {
         title: sessionStorage.getItem('title') || '',
         markdown: sessionStorage.getItem('markdown') || '',
+        markdownHeight: sessionStorage.getItem('markdownHeight') || '700',
+        markdownWidth: sessionStorage.getItem('markdownWidth') || '700',
     };
     
     $('#document-title').val(sessionData.title);
     $('#markdown').val(sessionData.markdown);
     $('#markdown').css('font-family', preferences.fontFamily);
     $('#markdown').css('font-size', parseInt(preferences.fontSize));
+    $('#markdown').css('height', parseInt(sessionData.markdownHeight));
+    $('#markdown').css('width', parseInt(sessionData.markdownWidth));
     $('#font-family').val(preferences.fontFamily);
     $('#font-size').val(preferences.fontSize);
     $('#output-format').val(preferences.format);
     
     // Save the users markdown on page refresh
     window.addEventListener('beforeunload', function (e) {
-        var markdown = $('#markdown').val();
-        sessionStorage.setItem('markdown', markdown);
+        var markdown = $('#markdown');
+        var markdownHeight = markdown.height();
+        var mardownWidth = markdown.width();
+        sessionStorage.setItem('markdown', markdown.val());
+        sessionStorage.setItem('markdownHeight', markdownHeight);
+        sessionStorage.setItem('markdownWidth', mardownWidth);
     });
 });
